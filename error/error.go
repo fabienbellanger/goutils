e1e7ff13cf7082a8cfdd09ad51c3cdc9a65288e7
Fabien Bellanger <valentil@gmail.com>
2020-01-03 21:55:42 +0100

    Changement de nom de fonction

--- error/error.go
+++ error/error.go
@@ -11,8 +11,8 @@ import (
 // color enables colors in console.
 var color aurora.Aurora = aurora.NewAurora(true)
 
-// CheckError manages errors
-func CheckError(err error, exitCode int) {
+// Check manages errors.
+func Check(err error, exitCode int) {
 	if err != nil {
 		if exitCode != 0 {
 			log.Printf(color.Sprintf(color.Red("Error(%v): %v"), strconv.Itoa(exitCode), err.Error()))
