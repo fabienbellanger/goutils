5ca7f58ba3cb29af48ea1f1f8d3288f89f9e8a92
Fabien Bellanger <valentil@gmail.com>
2020-01-09 20:53:09 +0100

    Ajout commentaires

--- time.go
+++ time.go
@@ -2,17 +2,19 @@ package goutils
 
 import "time"
 
-// StartOfDay returns a time
+// StartOfDay returns a the first time of a day.
 func StartOfDay(t time.Time) time.Time {
 	year, month, day := t.Date()
 	return time.Date(year, month, day, 0, 0, 0, 0, t.Location())
 }
 
+// EndOfDay returns a the last time of a day.
 func EndOfDay(t time.Time) time.Time {
 	year, month, day := t.Date()
 	return time.Date(year, month, day, 23, 59, 59, 999999999, t.Location())
 }
 
+// Yesterday returns yesterday time.
 func Yesterday() time.Time {
 	return time.Now().AddDate(0, 0, -1)
 }
