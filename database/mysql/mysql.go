7c4e4f9b6db6d82a99d3840c0394aeb281d5e1dd
Fabien Bellanger <valentil@gmail.com>
2020-03-23 22:01:41 +0100

    Mise à jour des modules Ajout d'un helper pour MySQL

---
+++ database/mysql/mysql.go
@@ -0,0 +1,143 @@
+package mysql
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql" // MySQL driver
+)
+
+var (
+	// DB is the connection handle
+	DB *sql.DB
+)
+
+// Open opens database connection
+func Open(user, password, host, port, name, timezone, charset string, maxOpenConns, maxIdleConns int, maxLifetime time.Duration) error {
+	db, err := sql.Open(
+		"mysql",
+		user+":"+password+"@tcp("+host+":"+port+")"+"/"+name+
+			"?parseTime=true&loc="+timezone+"&charset="+charset)
+	if err != nil {
+		return err
+	}
+
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetMaxIdleConns(maxIdleConns)
+	db.SetConnMaxLifetime(maxLifetime * time.Minute)
+
+	DB = db
+
+	return nil
+}
+
+// prepareQuery prepares query
+func prepareQuery(query string) (*sql.Stmt, error) {
+	statement, err := DB.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+
+	return statement, nil
+}
+
+// executeQuery executes request of type INSERT, UPDATE or DELETE
+func executeQuery(query string, args ...interface{}) (sql.Result, error) {
+	statement, err := prepareQuery(query)
+	if err != nil {
+		return nil, err
+	}
+	defer statement.Close()
+
+	result, err := statement.Exec(args...)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, err
+}
+
+// Select request
+func Select(query string, args ...interface{}) (*sql.Rows, error) {
+	statement, err := prepareQuery(query)
+	if err != nil {
+		return nil, err
+	}
+	defer statement.Close()
+
+	rows, err := statement.Query(args...)
+	if err != nil {
+		return nil, err
+	}
+
+	return rows, err
+}
+
+// Insert request
+func Insert(query string, args ...interface{}) (int64, error) {
+	result, err := executeQuery(query, args...)
+	if err != nil {
+		return 0, err
+	}
+
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	if err != nil {
+		return 0, err
+	}
+
+	return id, err
+}
+
+// Update request
+func Update(query string, args ...interface{}) (int64, error) {
+	result, err := executeQuery(query, args...)
+	if err != nil {
+		return 0, err
+	}
+
+	if err != nil {
+		return 0, err
+	}
+
+	affect, err := result.RowsAffected()
+	if err != nil {
+		return 0, err
+	}
+
+	if err != nil {
+		return 0, err
+	}
+
+	return affect, err
+}
+
+// Delete request
+func Delete(query string, args ...interface{}) (int64, error) {
+	result, err := executeQuery(query, args...)
+	if err != nil {
+		return 0, err
+	}
+
+	if err != nil {
+		return 0, err
+	}
+
+	affect, err := result.RowsAffected()
+	if err != nil {
+		return 0, err
+	}
+
+	if err != nil {
+		return 0, err
+	}
+
+	return affect, err
+}
--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module github.com/fabienbellanger/goutils
 
-go 1.13
+go 1.14
 
 require (
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/logrusorgru/aurora v0.0.0-20200102142835-e9ef32dff381
-	github.com/stretchr/testify v1.4.0
+	github.com/stretchr/testify v1.5.1
 )
